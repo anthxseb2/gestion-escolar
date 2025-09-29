6054fc87926cf1173078963cdd7240dbe906457d
anthxseb2 <antialvites03@gmail.com>
2025-09-29 05:41:41 +0200

    fix ruta / en app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola, esta es mi primera web en Render 🚀"
+    return "Hola Render 🚀"
 
 if __name__ == "__main__":
-    # Render usa el puerto de la variable de entorno PORT
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
