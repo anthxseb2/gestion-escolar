d7d4e71cf1f7337cd4c257c88309b4360a40e759
anthxseb2 <antialvites03@gmail.com>
2025-09-29 05:38:50 +0200

    Fix: usar puerto dinámico para Render

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-import os
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "¡Hola! 🚀 Tu app Flask está corriendo en Render."
+    return "Hola, esta es mi primera web en Render 🚀"
 
 if __name__ == "__main__":
-    # Render necesita que escuche en el puerto que él asigna
+    # Render asigna el puerto automáticamente
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
