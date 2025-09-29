4041300f2629588852cbab5efd4ff4393e02e589
anthxseb2 <antialvites03@gmail.com>
2025-09-29 05:45:24 +0200

    fix: ruta principal / en Flask para Render

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hola Render 🚀"
+def index():
+    return "<h1>¡Hola mundo desde Flask en Render 🚀!</h1>"
 
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
