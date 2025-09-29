6c5719f1321e00aae440669e2edf36358e8a94cc
anthxseb2 <antialvites03@gmail.com>
2025-09-29 05:31:45 +0200

    Actualizo app.py para Render

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Bienvenido al Proyecto de Gestión Escolar</h1><p>Esta es una web básica con Flask 🚀</p>"
+    return "¡Hola! 🚀 Tu app Flask está corriendo en Render."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Render necesita que escuche en el puerto que él asigna
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
